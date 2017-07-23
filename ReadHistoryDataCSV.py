dbc3a3af00a8b79bfadab95ba302a4f3890f899f
imkakac <ran.cheng28@gmail.com>
2017-07-23 06:53:45 -0400

    07/13/2017
    07/13/2017

--- ReadHistoryDataCSV.py
+++ ReadHistoryDataCSV.py
@@ -11,11 +11,14 @@ This is a temporary script file.
 import csv
 import pandas as pd
 import math
+import numpy as np
 
 folder = 'history data'
 #find all .csv file in the folder
 import glob
 csv_list = glob.glob(folder + '/*.csv')
+
+
 
 #read all .csv files and save as a dictionary
 history_data = {}
@@ -23,14 +26,33 @@ history_data = {}
 for i in range(0, len(csv_list)):
         #read history trip data from csv
         history_data[i] = pd.read_csv(csv_list[i])
+        # drop row with nan
+        history_data[i]=history_data[i].dropna(how='any') 
         if i < 19:
             #remove row 'Bike #'
             history_data[i].drop(history_data[i].columns[5], 1, inplace=True)
+#            df = df[np.isfinite(df['EPS'])]
+            
+#            if i == [0,1,2,4]:
+#                history_data[]
         else:
             #remove rows 'Start Station', 'end station', and 'Bike #'
             history_data[i].drop(history_data[i].columns[[4,6,7]], 1, inplace=True)
             
 import time
+import datetime
+
+for i in range(0,5):
+    temp = history_data[i]
+    func = lambda x: x.split("(", 1)[1].split(")", 1)[0]
+#    columns = ['Start station', 'End station']
+    #index = np.arange(103) # array of numbers for the number of samples
+#    teststring2 = pd.DataFrame(columns=columns)
+    temp['Start station'] = temp['Start station'].apply(func)
+    temp['End station'] = temp['End station'].apply(func)
+    history_data[i] = temp
+
+    
 
 aaa = history_data[0]
 datetimestring = history_data[0].iloc[0,1]
@@ -38,12 +60,34 @@ timestamp = time.mktime(time.strptime(datetimestring, '%m/%d/%Y %H:%M'))
 datetime.datetime.strptime(datetimestring, '%m/%d/%Y %H:%M').strftime('%A')
 timeobject = datetime.datetime.strptime(datetimestring, '%m/%d/%Y %H:%M')
 
-import datetime
+
 time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
 time.strftime('%Y', time.localtime(timestamp))
 time.strftime('%A', time.localtime(time.time()))
 time.gmtime(timestamp)
 time.gmtime(time.time())
+
+#create a function to extract the station number for history_data[0:2,4]
+testdf = history_data[0]
+#teststring = testdf.iloc[0,3]
+#Ind1 = testdf.iloc[0,3].find('(')
+#Ind2 = testdf.iloc[0,3].find(')')
+#StartStationNunmberTest = teststring = testdf.iloc[0,3][Ind1+1:Ind2]
+
+teststring = testdf.iloc[:,3:5]
+
+#Ind1 = testdf.iloc[:,3].find('(')
+#Ind2 = testdf.iloc[:,3].find(')')
+#StartStationNunmberTest = teststring = testdf.iloc[:,3][Ind1+1:Ind2]
+#teststring2 = teststring.apply(lambda x: x.split("(", 1)[1].split(")", 1)[0])
+func = lambda x: x.split("(", 1)[1].split(")", 1)[0]
+columns = ['Start station', 'End station']
+#index = np.arange(103) # array of numbers for the number of samples
+teststring2 = pd.DataFrame(columns=columns)
+teststring2['Start station'] = teststring['Start station'].apply(func)
+teststring2['End station'] = teststring['End station'].apply(func)
+
+#create a function to search the station number for history_data[3, 5:18]
 
 #testdf = history_data[0].iloc[0:10,0:2]
 #testdf['new_date']
